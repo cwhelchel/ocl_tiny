56df0b32d2611b52c1dfd0e6835724c2e8f9559c
Cainan Whelchel <c.whelchel@gmail.com>
2012-08-25 19:15:59 -0400

    minor: comments about this translation
    this omp prog may not be able to be easily translated into ocl

--- ocl_tiny05/ocl_tiny05/ocl_tiny05.cpp
+++ ocl_tiny05/ocl_tiny05/ocl_tiny05.cpp
@@ -6,6 +6,9 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+    // tiny05 omp may not be translatable without a large
+    // re-write of the programin in ocl. the omp algorithm
+    // relies on the omp critical section which ocl doesn't have.
 	return 0;
 }
 
